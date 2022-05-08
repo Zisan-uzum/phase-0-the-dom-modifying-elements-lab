d5043c2f0f9c13b472f8df58aca931fa88cdf2f5
Zisan-uzum <uzummzisann@gmail.com>
2022-05-08 15:17:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+document.getElementById("main").remove();
+document.createElement("h1");
+document.getElementsByTagName("h1")[0].id = "victory";
+document.getElementById("victory").textContent = "ZISAN is the champion";
